1104adeeefccfcfdca83e2ed42ca079482d9d52b
Steven Truesdell <steven@strues.io>
2017-04-03 18:40:09 -0600

    fix build steps

--- internal/jest/transform.js
+++ internal/jest/transform.js
@@ -5,5 +5,7 @@ module.exports = babelJest.createTransformer({
   presets: ['stage-2', 'es2015', 'react'],
   plugins: [
     'transform-decorators-legacy',
+    'transform-class-properties',
+    'transform-object-rest-spread'
   ],
 });
