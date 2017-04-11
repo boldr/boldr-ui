8aa72b882bfeafc396cb5ce53b27bf6eac9819e1
Steven Truesdell <steven@strues.io>
2017-04-11 11:23:29 -0600

    adjust alignment on sidebar icons and text. provide ability to customize all topbar links

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import { Dropdown, DropdownMenu, DropdownItem } from 'reactstrap';
 import { Link, withRouter } from 'react-router-dom';
+import Icon from '../Icon';
 
 type Props = {
   toggle: () => void,
   links: Array<Object>,
+  userLinks: Array<Object>,
+  rightLinks: Array<Object>,
+  user: Object,
 };
 
 class Topbar extends Component {
+  static defaultProps = {
+    user: {
+      avatarUrl: 'https://boldr.io/images/unknown-avatar.png',
+      username: 'blank',
+    },
+  };
+
   constructor(props) {
     super(props);
 
@@ -34,7 +45,7 @@ class Topbar extends Component {
   }
   props: Props;
   render() {
-    const { links } = this.props;
+    const { links, rightLinks, user, userLinks } = this.props;
     return (
       <header className="boldrui-dash-topbar navbar">
         <button
@@ -45,114 +56,61 @@ class Topbar extends Component {
           ☰
         </button>
         <a className="navbar-brand" href="#" />
-          {links &&
-            <ul className="nav navbar-nav hidden-md-down">
-              <li className="nav-item">
-                <a
-                  className="nav-link navbar-toggler sidebar-toggler"
-                  onClick={this.sidebarToggle}
-                  href="#"
-                >
-                  ☰
-                </a>
+        {
+          !links &&
+          <a className="boldrui-sidebar-toggler__alone navbar-toggler sidebar-toggler" onClick={ this.sidebarToggle } href="#">
+            ☰
+          </a>
+        }
+        {links &&
+          <ul className="nav navbar-nav hidden-md-down">
+            <li className="nav-item">
+              <a className="nav-link navbar-toggler sidebar-toggler" onClick={ this.sidebarToggle } href="#">
+                ☰
+              </a>
+            </li>
+            {links.map(lnk => (
+              <li className="nav-item px-1" key={ lnk.key }>
+                <Link className="nav-link" to={ lnk.link }>{lnk.text}</Link>
               </li>
-              {links.map(lnk => (
-                <li className="nav-item px-1">
-                  <Link className="nav-link" to={ lnk.link }>{ lnk.text }</Link>
-                </li>
-              ))}
-            </ul>}
-        <ul className="nav navbar-nav ml-auto">
-          <li className="nav-item hidden-md-down">
-            <a className="nav-link" href="#">
-            temp
-            </a>
-          </li>
-          <li className="nav-item hidden-md-down">
-            <a className="nav-link" href="#"><i className="icon-list" /></a>
-          </li>
-          <li className="nav-item hidden-md-down">
-            <a className="nav-link" href="#">
-              <i className="icon-location-pin" />
-            </a>
-          </li>
+            ))}
+          </ul>}
+        <ul className="boldrui-dash-topbar-right nav navbar-nav ml-auto">
+          {
+            rightLinks &&
+            rightLinks.map(rlink => (
+              <li className="nav-item hidden-md-down" key={ rlink.key }>
+                <Link to={ rlink.link } className="nav-link">
+                  { rlink.text }
+                </Link>
+              </li>
+            ))
+          }
           <li className="nav-item">
             <Dropdown isOpen={ this.state.dropdownOpen } toggle={ this.toggle }>
               <a
-                onClick={this.toggle}
+                onClick={ this.toggle }
                 className="nav-link dropdown-toggle nav-link"
                 data-toggle="dropdown"
                 href="#"
                 role="button"
                 aria-haspopup="true"
-                aria-expanded={this.state.dropdownOpen}
+                aria-expanded={ this.state.dropdownOpen }
               >
-                <img
-                  src="https://boldr.io/images/unknown-avatar.png"
-                  className="img-avatar"
-                  alt="admin user ava"
-                />
-                <span className="hidden-md-down">admin</span>
+                <img src={ user.avatarUrl } className="img-avatar" alt="admin user ava" />
+                <span className="hidden-md-down">{ user.username }</span>
               </a>
 
               <DropdownMenu className="dropdown-menu-right">
-                <DropdownItem header className="text-center">
-                  <strong>Account</strong>
-                </DropdownItem>
-
-                <DropdownItem>
-                  <i className="fa fa-bell-o" />
-                  {' '}
-                  Updates
-                  <span className="badge badge-info">42</span>
-                </DropdownItem>
-                <DropdownItem>
-                  <i className="fa fa-envelope-o" />
-                  {' '}
-                  Messages
-                  <span className="badge badge-success">42</span>
-                </DropdownItem>
-                <DropdownItem>
-                  <i className="fa fa-tasks" />
-                  {' '}
-                  Tasks
-                  <span className="badge badge-danger">42</span>
-                </DropdownItem>
-                <DropdownItem>
-                  <i className="fa fa-comments" />
-                  {' '}
-                  Comments
-                  <span className="badge badge-warning">42</span>
-                </DropdownItem>
-
-                <DropdownItem header className="text-center">
-                  <strong>Settings</strong>
-                </DropdownItem>
-
-                <DropdownItem>
-                  <i className="fa fa-user" /> Profile
-                </DropdownItem>
-                <DropdownItem>
-                  <i className="fa fa-wrench" /> Settings
-                </DropdownItem>
-                <DropdownItem>
-                  <i className="fa fa-usd" />
-                  {' '}
-                  Payments
-                  <span className="badge badge-default">42</span>
-                </DropdownItem>
-                <DropdownItem>
-                  <i className="fa fa-file" />
-                  {' '}
-                  Projects
-                  <span className="badge badge-primary">42</span>
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  <i className="fa fa-shield" /> Lock Account
-                </DropdownItem>
-                <DropdownItem><i className="fa fa-lock" /> Logout</DropdownItem>
-
+                {
+                  userLinks &&
+                  userLinks.map(ulink => (
+                    <DropdownItem key={ ulink.key }>
+                      <Icon kind={ ulink.icon } color={ ulink.iconColor } />
+                      <Link to={ ulink.link }>{ ulink.text }</Link>
+                    </DropdownItem>
+                  ))
+                }
               </DropdownMenu>
             </Dropdown>
           </li>
