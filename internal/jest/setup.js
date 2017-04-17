c2fa51a5456dbec403d230f98461bf90e571511b
Steven Truesdell <steven@strues.io>
2017-04-17 13:31:15 -0600

    test(link): fix linnk test (#2)

--- internal/jest/setup.js
+++ internal/jest/setup.js
@@ -8,14 +8,9 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
 global.shallow = shallow;
 global.render = render;
-global.render = mount;
+global.mount = mount;
 global.shallowToJson = shallowToJson;
-// Skip createElement warnings but fail tests on any other warning
-console.error = message => {
-  if (!/(React.createElement: type should not be null)/.test(message)) {
-    throw new Error(message);
-  }
-};
+
 window.matchMedia = function matchMedia() {
   return false;
 };
